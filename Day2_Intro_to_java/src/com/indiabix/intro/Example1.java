a52a9857dada86d696603c32d485a6ad46ed9032
Namratha <namrathamsomani21@gmail.com>
2022-05-16 00:56:51 +0530

    First commit

---
+++ Day2_Intro_to_java/src/com/indiabix/intro/Example1.java
@@ -0,0 +1,22 @@
+//compile time input
+package com.indiabix.intro;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=15;//whole
+float b=10.5f;//deci precision upto 6 dec places
+double c=14.7;//deci stores upto 15 dec places
+char d='x';
+String str="HII";
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(str);
+
+
+	}
+
+}
---
+++ Day2_Intro_to_java/src/com/indiabix/intro/Example2.java
@@ -0,0 +1,15 @@
+//runtime ip
+package com.indiabix.intro;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+System.out.println(a);
+sc.close();//used after scanner statements are used
+	}
+
+}
